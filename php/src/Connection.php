18d0b8bace87dfd2bf324472897da7b1a6d44fee
Ntabethemba Mabetshe <bmaneza@gmail.com>
2023-10-16 11:33:46 +0000

    The database

---
+++ php/src/Connection.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * @package   Task Management
+ * @author    Ntabethemba Ntshoza
+ * @date      11-10-2023
+ * @copyright Copyright © 2023 VMP By Maneza
+ */
+
+if (session_id() === '') {
+    session_start();
+} else {
+    session_start();
+}
+
+
+// Database credentials
+$dbHost = 'db';
+$dbName = 'ekomi';
+$dbUser = 'admin';
+$dbPassword = 'admin124';
+
+$db = new mysqli($dbHost, $dbUser, $dbPassword, $dbName);
+
+if ($db->connect_error) {
+    die("Connection failed: " . $db->connect_error);
+}
