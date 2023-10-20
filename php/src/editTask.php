f29c7e113bb6337d3c8b61619dbe32db4fe1db34
Ntabethemba Mabetshe <bmaneza@gmail.com>
2023-10-20 07:36:00 +0000

    Adding favicon image...

--- php/src/editTask.php
+++ php/src/editTask.php
@@ -88,7 +88,10 @@ if (isset($_GET['id'])) {
 <head>
     <meta charset="utf-8">
     <title>Edit Task - eKomi Tasks management</title>
-    <link rel="icon" type="image/x-icon" href="images/fav.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="images/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="images/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="images/favicon-16x16.png">
+    <link rel="manifest" href="images/site.webmanifest">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Responsive Minimal Bootstrap Theme">
     <meta name="keywords" content="responsive,minimal,bootstrap,theme">
--- php/src/forgotPassword.php
+++ php/src/forgotPassword.php
@@ -36,7 +36,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Forgot Password</title>
-    <link rel="icon" type="image/x-icon" href="images/fav.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="images/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="images/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="images/favicon-16x16.png">
+    <link rel="manifest" href="images/site.webmanifest">
     <!-- Include your CSS and Bootstrap links here -->
 
     <!-- Include Bootstrap CSS -->
